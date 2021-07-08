d6614b0ac3fbea4e4816111de7d57c858ead40a4
RBReif <reifr@in.tum.de>
2021-07-08 17:55:36 +0200

    Filtering of Appointments for Patients need

--- controllers/appointment.js
+++ controllers/appointment.js
@@ -9,6 +9,10 @@ const Enum = require("../src/enums")
 const createAppointment = async (req, res) => {
 
     try {
+        let doctor = await DoctorModel.findById(req.body.doctor)
+        if (doctor == null) {
+            return res.status(400).json({message: "The doctor ID does not belong to a stored doctor."})
+        }
         let appointment = await AppointmentModel.create(req.body);
         res.status(201).json(appointment)
     } catch (err) {
@@ -32,7 +36,7 @@ const updateAppointment = async (req, res) => {
                 if (req.body.patient == null) {
                     return res.status(400).json({message: "The patient ID was not specified"})
                 }
-                let patient = await DoctorModel.findById(req.params.patient)
+                let patient = await PatientModel.findById(req.params.patient)
                 if (patient == null) {
                     return res.status(400).json({message: "The patient ID does not belong to a stored patient."})
                 }
@@ -94,41 +98,68 @@ const filterAppointment = async (req, res) => {
         let profession = req.body.profession
 
        if (!Object.values(Enum.AreaOfExpertise).includes(profession)) {
-            res.status(400).json({message: "Profession unknown"})
+          return   res.status(400).json({message: "Profession unknown"})
         }
 
 
         let languages = req.body.languagesD
         if (!languages.every((item) => Object.values(Enum.Language).includes(item))) {
-            res.status(400).json({message: "Language(s) unknown"})
+          return  res.status(400).json({message: "Language(s) unknown"})
         }
 
 
-        let facilities = req.body.facilities
+        let facilities
+        facilities= req.body.facilities
         if (!facilities.every((item) => Object.values(Enum.SpecialFacility).includes(item))) {
-            res.status(400).json({message: "Facilitie(s) unknown"})
+          return  res.status(400).json({message: "Facilitie(s) unknown"})
         }
-        console.log("Facilities: ",facilities)
-        console.log("Languages: ",languages)
+
+        console.log("Facilities: ", facilities)
+        console.log("Languages: ", languages)
+
+        let fittingDoctors
+            if (facilities.length>0){
+            fittingDoctors = await DoctorModel.find({
+            area_of_expertise: profession,
+            languages: {$in: languages},
+            special_facilities: {$all: facilities},
+        });}else{
+                fittingDoctors = await DoctorModel.find({
+                    area_of_expertise: profession,
+                    languages: {$in: languages},
+                });
+            }
+        console.log(fittingDoctors)
+
+        let fittingDoctorIDs = fittingDoctors.map((item) => item["_id"])
+
 
         let startpoint = new Date(req.body.startpoint)
         let endpoint = new Date(req.body.endpoint)
         if (startpoint.isNaN || endpoint.isNaN) {
-         //   res.status(500).json({message: "Date or Time wrongly specified"})
+          return  res.status(400).json({message: "Date or Time wrongly specified"})
+        }
+        if (endpoint<startpoint){
+            return   res.status(400).json({message: "Endpoint must be later than startpoint"})
+
         }
         console.log("Startpoint: ",startpoint)
         console.log("endpoint: ",endpoint)
 
+        let appointments = await AppointmentModel.find({
+            doctor: {$in: fittingDoctorIDs},
+            startPoint:{
+                $gte: startpoint,
+                $lt: endpoint
+            },
+            appointmentStatus: Enum.AppointmentStatus.AVAILABLE
 
-        let fittingDoctors = await DoctorModel.find({
-            area_of_expertise: profession,
-            languages: {$in: languages},
-            special_facilities: {$all: facilities}
         })
-        console.log(fittingDoctors)
-        let fittingDoctorIDs = fittingDoctors.map((item) => item["_id"])
 
-        res.status(200).json(fittingDoctors)
+        console.log("Appointments ", appointments)
+
+
+       return  res.status(200).json(appointments)
 
     } catch (err) {
         console.log(err)
