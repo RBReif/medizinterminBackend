d65f3dc4fd14013624e32ec99195ea352f3eaa66
Maximilian Pfleger <maximilian.pfleger@tum.de>
2021-07-20 19:07:23 +0200

    add thumbnail for patient

--- models/patient.js
+++ models/patient.js
@@ -3,7 +3,7 @@
 const mongoose = require("mongoose");
 // this is that the later created virtuals are included in the json send to the user
 const Enum = require("../src/enums")
-const opts = { toJSON: { virtuals: true } };
+const opts = {toJSON: {virtuals: true}};
 const AddressSchema = require("../models/address")
 
 /***
@@ -15,47 +15,49 @@ const AddressSchema = require("../models/address")
  */
 const PatientSchema = new mongoose.Schema({
 
-    // username of patient
-    username:{
+        // username of patient
+        username: {
             type: String,
             required: true,
             unique: true,
         },
-    // password of patient
-    password: {
+        // password of patient
+        password: {
             type: String,
             required: true,
         },
-    // firstname of patient
-    firstname: {
-        type: String,
-        required: true,
+        // firstname of patient
+        firstname: {
+            type: String,
+            required: true,
+        },
+        // lastname of patient
+        lastname: {
+            type: String,
+            required: true,
+        },
+        // date of birth
+        date_of_birth: {
+            type: Date,
+            required: true,
+        },
+        // insurance
+        insurance: {
+            type: String,
+            enum: Enum.InsuranceType,
+            required: true,
+        },
+        address: {
+            type: AddressSchema,
+            require: true,
+        },
+        gender: {
+            type: String,
+            required: true,
+        },
+        thumbnail: String,
+
     },
-    // lastname of patient
-    lastname: {
-        type: String,
-        required: true,
-    },
-    // date of birth
-    date_of_birth: {
-        type: Date,
-        required: true,
-    },
-    // insurance
-    insurance: {
-        type: String,
-        enum: Enum.InsuranceType,
-        required: true,
-    },
-    address: {
-        type: AddressSchema,
-        require: true,
-    },
-    gender: {
-        type: String,
-        required: true,
-    },
-},
     {collection: 'patient'});
 // Export the Patient model
 module.exports = mongoose.model("patient", PatientSchema);
